11f42b13fa475fd9142dab08999c6002c609df6c
NouranElhamdy <nouranelhamdy@gmail.com>
2023-05-09 15:41:09 +0300

    [Add] Needed scripts

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace KAlabs
+{
+   public class UIManager : MonoBehaviour
+    {
+        #region Public Variables
+        #endregion
+
+        #region Private Variables
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Unity Callbacks
+        #endregion
+
+        #region Event Callbacks
+        #endregion
+
+        #region Unity GUI Callbacks
+        #endregion
+
+        #region Public Methods
+        #endregion
+
+        #region Private Methods
+        #endregion
+
+        #region Coroutines
+        #endregion
+    }
+
+}
